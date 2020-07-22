ca15ca97b405b57e4f3d6a5c62f01a30af0789b8
Austin Carly <austinrcarly16@gmail.com>
2020-07-22 10:36:12 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
+import React from "react"
+import List from "./Components/List"
+import "./App.css"
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className='movie-list-title'> The Most Amazing Movie List</h1>
+            <List />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default App
 
